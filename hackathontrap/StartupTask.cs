1845c12a141202b2c72e713b762bff1014411d6e
adam grocholski <adgroc@outlook.com>
2016-05-25 08:18:28 -0500

    loop to receive commands

--- hackathontrap/StartupTask.cs
+++ hackathontrap/StartupTask.cs
@@ -16,38 +16,52 @@ namespace hackathontrap
     public sealed class StartupTask : IBackgroundTask
     {
         private BackgroundTaskDeferral _deferral;
+        private string _deviceId = "hackathontrap";
+        private string _deviceKey = "BaBrZjE2nnpUIQ+QiG9rJVy4zWhdNmaqw9DrckrM05s=";
+        private string _ioTHubHostName = "iothubhackathon.azure-devices.net";
+                
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             _deferral = taskInstance.GetDeferral();
             var cts = new CancellationTokenSource();
             var taskSending = TelemetrySendLoop(cts.Token);
+            var taskReceive = ReceiveCommandLoop(cts.Token);
+        }
+
+        static async Task ReceiveCommandLoop(CancellationToken ct)
+        {
+                var deviceClient = GetDeviceClient();
+                
+                while (!ct.IsCancellationRequested)
+                {
+                    Message receivedMessage = await Deviceclient.ReceiveAsync();
+                    
+                    if (receivedMessage == null) continue;
+                    
+                    var messageText = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    
+                    //TODO: perform some action based on message
+                    
+                    await Deviceclient.CompleteAsync(receivedMessage);
+                }
         }
 
         static async Task TelemetrySendLoop(CancellationToken ct)
         {
             try
-            {
-                // Get DeviceId,DeviceKey & IoTHubHostName from config.
-                var DeviceId = "hackathontrap";
-                var DeviceKey = "BaBrZjE2nnpUIQ+QiG9rJVy4zWhdNmaqw9DrckrM05s=";
-                var IoTHubHostName = "iothubhackathon.azure-devices.net";
-                
-                // Generate the symmetric key
-                var authMethod = new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey);
-
-                // Build the connection string
-                var connecitonString = IotHubConnectionStringBuilder.Create(IoTHubHostName, authMethod).ToString();
-                var Deviceclient = DeviceClient.CreateFromConnectionString("HostName=iothubhackathon.azure-devices.net;DeviceId=hackathontrap;SharedAccessKey=BaBrZjE2nnpUIQ+QiG9rJVy4zWhdNmaqw9DrckrM05s=", TransportType.Http1);
+            {                
+                var deviceClient = GetDeviceClient();
 
                 while (!ct.IsCancellationRequested)
                 {
 
                     var messageObject = new
                     {
-                        DeviceID = DeviceId,
+                        DeviceID = _deviceId,
                         Temperature = 34.2,
                         ExternalTemperature = 38.7,
-                        Humidity = 37.7
+                        Humidity = 37.7,
+                        Timestamp = Datetime.Now
                     };
 
                     // to Json
@@ -63,6 +77,18 @@ namespace hackathontrap
             {
                 var message = ex.Message;
             }
+        }
+        
+        static DeviceClient GetDeviceClient()
+        {
+                // Generate the symmetric key
+                var authMethod = new DeviceAuthenticationWithRegistrySymmetricKey(_deviceId, _deviceKey);
+
+                // Build the connection string
+                var connecitonString = IotHubConnectionStringBuilder.Create(_ioTHubHostName, authMethod).ToString();
+                var deviceClient = DeviceClient.CreateFromConnectionString("HostName=iothubhackathon.azure-devices.net;DeviceId=hackathontrap;SharedAccessKey=BaBrZjE2nnpUIQ+QiG9rJVy4zWhdNmaqw9DrckrM05s=", TransportType.Http1);
+                
+                return deviceClient
         }
     }
 }
